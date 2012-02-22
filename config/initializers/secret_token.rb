42b45d377d8402c527af7376d821b1edfafd0e93
roi <roi@roi-VirtualBox.(none)>
2012-02-22 23:33:14 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f30ad0fcf60be9539dd6312ba506efe8605b5b6787f3f6a41bc6cf5834cfe82bf87a4182126dacdd4d64c8dc2f48748a5691c3fd3840e46a2c9d85d7864d72ec'
